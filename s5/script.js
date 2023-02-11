8b147aa10078b9f038b619db47938f1c65b7c402
Aymane <aymanefdl1@gmail.com>
2023-02-11 22:28:17 +0100

    update 2.0
    adding s5 and 6 without others

---
+++ s5/script.js
@@ -0,0 +1,23 @@
+    function bd(){
+        window.open("https://ecampus-fssm.uca.ma/enrol/index.php?id=390");
+    }
+    
+    function oop(){
+        window.open("https://ecampus-fssm.uca.ma/enrol/index.php?id=395");
+    }
+    
+    function rech_op(){
+        window.open("https://ecampus-fssm.uca.ma/enrol/index.php?id=393");
+    }
+    
+    function reseaux(){
+        window.open("https://ecampus-fssm.uca.ma/enrol/index.php?id=392");
+    }
+    
+    function sys_info(){
+        window.open("https://ecampus-fssm.uca.ma/enrol/index.php?id=394");
+    }
+
+    function compilation(){
+        window.open("https://ecampus-fssm.uca.ma/enrol/index.php?id=42");
+    }
---
+++ s6/script.js
@@ -0,0 +1,15 @@
+    function bd2(){
+        window.open("https://ecampus-fssm.uca.ma/enrol/index.php?id=391");
+    }
+    
+    function php(){
+        window.open("https://ecampus-fssm.uca.ma/enrol/index.php?id=45");
+    }
+    
+    function ia(){
+        window.open("https://ecampus-fssm.uca.ma/enrol/index.php?id=44");
+    }
+    
+    function uml(){
+        window.open("https://ecampus-fssm.uca.ma/enrol/index.php?id=43");
+    }
