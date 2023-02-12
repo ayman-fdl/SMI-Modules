8463c6554197b3e4f938d37e1134929f3f9fca57
Aymane <aymanefdl1@gmail.com>
2023-02-12 23:42:13 +0100

    s3-update

---
+++ s3/script.js
@@ -0,0 +1,23 @@
+function algo(){
+        window.open("https://ecampus-fssm.uca.ma/course/view.php?id=385");
+    }
+    
+    function prog(){
+        window.open("https://ecampus-fssm.uca.ma/course/view.php?id=384");
+    }
+    
+    function techno(){
+        window.open("https://ecampus-fssm.uca.ma/course/view.php?id=388");
+    }
+    
+    function sys(){
+        window.open("https://ecampus-fssm.uca.ma/course/view.php?id=386");
+    }
+    
+    function pro(){
+        window.open("https://ecampus-fssm.uca.ma/course/view.php?id=387");
+    }
+
+    function elect(){
+        window.open("https://ecampus-fssm.uca.ma/course/view.php?id=389");
+    }
