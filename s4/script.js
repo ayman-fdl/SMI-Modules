0905ada114303c7d18550ac64590a138e04ad0c3
Aymane <aymanefdl1@gmail.com>
2023-02-12 17:42:49 +0100

    s4-update

---
+++ s4/script.js
@@ -0,0 +1,23 @@
+    function prog(){
+        window.open("https://ecampus-fssm.uca.ma/enrol/index.php?id=708");
+    }
+    
+    function analyse(){
+        window.open("https://ecampus-fssm.uca.ma/course/view.php?id=715");
+    }
+    
+    function struc(){
+        window.open("https://ecampus-fssm.uca.ma/enrol/index.php?id=40");
+    }
+    
+    function electro(){
+        window.open("https://ecampus-fssm.uca.ma/enrol/index.php?id=294");
+    }
+    
+    function sys2(){
+        window.open("https://ecampus-fssm.uca.ma/enrol/index.php?id=41");
+    }
+
+    function ord(){
+        window.open("https://ecampus-fssm.uca.ma/enrol/index.php?id=38");
+    }
